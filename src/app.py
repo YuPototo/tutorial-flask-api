743bbb993bba90b85de1fc52be0c035ee0df3f13
Yu Qin <sirius.qinyu@gmail.com>
2019-06-27 10:44:47 +0800

    add uwsgi.ini

--- src/app.py
+++ src/app.py
@@ -13,12 +13,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False # 关掉 flask-sqlalchemy �
 app.config['SECRET_KEY'] = 'super-secret'
 api = Api(app)
 
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
-
 jwt = JWT(app, authenticate, identity)
 
 api.add_resource(Store, '/store/<string:name>')
